a132a7b34927071d3399899230a99592185e6ba1
= <peaplant4@gmail.com>
2024-11-28 14:27:14 +0900

    fixing some bug

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function buytpc() {
     if (gameData.thing >= gameData.tpcCost) {
         gameData.thing -= gameData.tpcCost
         gameData.tpc += 1
-        gameData.tpcCost = gameData.tpcCost * 1.5
+        gameData.tpcCost = Math.floor(gameData.tpcCost * 1.5)
         gameData.tpcLevel += 1
         document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
         document.getElementById('tpcUpdate').innerHTML = 'Thing gain upgrade: level ' + gameData.tpcLevel + '<br>(Cost:' + gameData.tpcCost +')'
@@ -31,8 +31,8 @@ var mainGameLoop = window.setInterval(function() {refreshing()}, 50)
 
 var savegame = JSON.parse(localStorage.getItem("NISave"))
 if (savegame !== null) {
-  gameData = savegame
-  document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
-  document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
-        document.getElementById('tpcUpdate').innerHTML = 'Thing gain upgrade: level ' + gameData.tpcLevel + '<br>(Cost:' + gameData.tpcCost +')'
+    gameData = savegame
+    document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
+    document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
+    document.getElementById('tpcUpdate').innerHTML = 'Thing gain upgrade: level ' + gameData.tpcLevel + '<br>(Cost:' + gameData.tpcCost +')'
 }
