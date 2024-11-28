cbc6fe80142a9b483949cffa9cb69826ace413d1
= <peaplant4@gmail.com>
2024-11-28 14:22:08 +0900

    Updating game saving system

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 var gameData = {
     thing: 0,
     tpc: 1, //things per click
-    tpcCost: 10
+    tpcCost: 10,
+    tpcLevel: 0
 }
 
 function getThing() {
     gameData.thing += gameData.tpc
-    if(gameData.thing === 0) {
-        document.getElementById('thingCurrent').innerHTML = 'Nothing: ' + gameData.thing
-    } else {
-        document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
-    }
+    document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
+
 }
 
 function buytpc() {
     if (gameData.thing >= gameData.tpcCost) {
         gameData.thing -= gameData.tpcCost
-        gameData.tpc *= 2
-        gameData.tpcCost = gameData.tpcCost * 2 + 1
+        gameData.tpc += 1
+        gameData.tpcCost = gameData.tpcCost * 1.5
+        gameData.tpcLevel += 1
         document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
-        document.getElementById('tpcUpdate').innerHTML = 'Thing gain upgrade: level ' + gameData.tpc - 1 + '<br>(Cost:' + gameData.tpcCost +')'
+        document.getElementById('tpcUpdate').innerHTML = 'Thing gain upgrade: level ' + gameData.tpcLevel + '<br>(Cost:' + gameData.tpcCost +')'
     }
 }
 
-//var mainGameLoop = window.setInterval(function() {
-//  getThing()
-//}, 50)
-
 var saveGameLoop = window.setInterval(function() {
     localStorage.setItem("NISave", JSON.stringify(gameData))
-}, 15000)
+}, 10000)
+
+function refreshing() { gameData.thing += 0}
+var mainGameLoop = window.setInterval(function() {refreshing()}, 50)
 
 var savegame = JSON.parse(localStorage.getItem("NISave"))
 if (savegame !== null) {
   gameData = savegame
+  document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
+  document.getElementById('thingCurrent').innerHTML = 'Thing: ' + gameData.thing
+        document.getElementById('tpcUpdate').innerHTML = 'Thing gain upgrade: level ' + gameData.tpcLevel + '<br>(Cost:' + gameData.tpcCost +')'
 }
